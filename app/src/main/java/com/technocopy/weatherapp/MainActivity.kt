4a9d9475c6536cd401b004b24ccde1796cf9d900
Sergi <technocopy@mail.ru>
2021-02-26 16:05:21 +0300

    Урок 1 знакомство с Котлин

--- app/src/main/java/com/technocopy/weatherapp/MainActivity.kt
+++ app/src/main/java/com/technocopy/weatherapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.technocopy.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    // создание глобальной переменной
+    var tvText : TextView? = null  // способ через null
+
+    lateinit var tvCount : TextView
+    lateinit var button: Button
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvText = findViewById(R.id.tvText)
+        tvText?.text = getString(R.string.start)
+
+        tvCount = findViewById(R.id.tvCount)
+        tvCount.text = count.toString()
+
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val myDialogFragment = MyDialogFragment()
+            val manager = supportFragmentManager
+            myDialogFragment.show(manager, "Нажата кнопка")
+
+            count++
+            tvCount.text = count.toString()
+        }
+
     }
+
 }
