d9d96d39eb3da34df102272fd166b46c5ba3a379
Sergi <technocopy@mail.ru>
2021-03-04 13:38:37 +0300

    3 подключено меню навигации. Исправлены ошибки в bottom_nav.xml

--- app/src/main/java/com/technocopy/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/technocopy/weatherapp/view/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.technocopy.weatherapp.R
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +18,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
 
         navController = Navigation.findNavController(this , R.id.nav_host_fragment)
 
+        bottom_nav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return when(navController.currentDestination?.id) {
+            R.id.currentWeatherFragment -> {
+                true
+            }
+            else -> navController.navigateUp()
+        }
+    }
+
 
 }
