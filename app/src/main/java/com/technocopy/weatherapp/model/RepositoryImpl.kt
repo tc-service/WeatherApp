d418facb89dd58a3c1b9d6c0d01f0f519d7376bb
Sergi <technocopy@mail.ru>
2021-03-09 13:00:39 +0300

    3.1 Урок 3 Создание списка городов

---
+++ app/src/main/java/com/technocopy/weatherapp/model/RepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.technocopy.weatherapp.model
+
+class RepositoryImpl : Repository {
+
+    override fun getWeatherFromServer(): Weather {
+        return Weather()
+    }
+
+    override fun getWeatherFromLocalStorageRus(): List<Weather> {
+        return getRussianCities()
+    }
+
+    override fun getWeatherFromLocalStorageWorld(): List<Weather> {
+        return getWorldCities()
+    }
+}
--- app/src/main/java/com/technocopy/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/technocopy/weatherapp/view/MainActivity.kt
@@ -17,6 +17,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, CurrentWeatherFragment.newInstance())
+                .commitAllowingStateLoss()
+        }
 
         setSupportActionBar(toolbar)
 
